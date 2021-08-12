dd4a4de022f59e87d213f3a3f9127271042e2aaa
PrathamRawat <prathamrawat815@gmail.com>
2021-08-12 17:35:16 -0400

    Add simple page to apply for loan

---
+++ site/src/components/LoanApplication.jsx
@@ -0,0 +1,29 @@
+import {useEffect, useRef} from "react";
+import {applyForLoan} from "../utils/database";
+
+
+function LoanApplication() {
+
+    const loanAmount = useRef(HTMLInputElement)
+    const loanDescription = useRef(HTMLTextAreaElement)
+
+    const apply = () => {
+        if(loanAmount == null || loanDescription == null) return
+        //TODO: Implement real username
+        applyForLoan("username", {
+            amount: loanAmount.current.value,
+            description: loanDescription.current.value
+        })
+        //TODO: redirect to confirmation page
+    }
+
+    return (<div>
+        <p>How much do you need to loan?</p> <input type={"text"} ref={loanAmount}/>
+        <p>Please describe what you need this loan for:</p>
+        <textarea ref={loanDescription}/>
+        <button onClick={apply}>Submit Loan Application</button>
+    </div>)
+
+}
+
+export default LoanApplication;
