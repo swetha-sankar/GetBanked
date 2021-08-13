2dd3017c064c73eba0558cd8ef6eaf3ba5136f73
swetha-sankar <swetha@udel.edu>
2021-08-12 22:45:23 -0400

    some more changes on the footer

--- site/src/components/Home.jsx
+++ site/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import logo from '../assets/c1logo.png';
-import {Box} from '@material-ui/core';
+import {Box, Button, Typography} from '@material-ui/core';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -14,10 +14,10 @@ function Home() {
             <header className="App-header">
                 <Box borderRadius = {10} > Loans reimagined </Box>
                 <img src={logo} className="App-logo" alt="capital-one-logo"/>
-                <p>You've invested $ {data} </p>
-                <button onClick={() => setData(data + 1)}>
+                <Typography>You've invested $ {data} </Typography>
+                <Button onClick={() => setData(data + 1)}>
                     Click here and invest a dollar
-                </button>
+                </Button>
             </header>
             <Footer/>
         </div>
